971d32b0ddccd33509d8083eefc5109fab5a915b
ranamandour-04 <rana4mahamad@gmail.com>
2026-08-06 02:32:35 +0300

    3

--- src/App.jsx
+++ src/App.jsx
@@ -648,34 +648,37 @@ function App() {
   const { siteInfo, posts, categories } = data;
   const { social } = data.siteInfo;
 
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <RootLayout social={social} />,
-      children: [
-        {
-          index: true,
-          element: <Home data={posts} category={categories} />,
-        },
-        {
-          path: "blog",
-          element: <Blog data={posts} category={categories} />,
-        },
-        {
-          path: "blog/:id",
-          element: <BlogDetails data={posts} />,
-        },
-        {
-          path: "about",
-          element: <About />,
-        },
-        {
-          path: "*",
-          element: <NotFound />,
-        },
-      ],
-    },
-  ]);
+  const router = createBrowserRouter(
+    [
+      {
+        path: "/",
+        element: <RootLayout social={social} />,
+        children: [
+          {
+            index: true,
+            element: <Home data={posts} category={categories} />,
+          },
+          {
+            path: "blog",
+            element: <Blog data={posts} category={categories} />,
+          },
+          {
+            path: "blog/:id",
+            element: <BlogDetails data={posts} />,
+          },
+          {
+            path: "about",
+            element: <About />,
+          },
+          {
+            path: "*",
+            element: <NotFound />,
+          },
+        ],
+      },
+    ],
+    { basename: "/lens" },
+  );
   return (
     <>
       <RouterProvider router={router} />
